520b4c1fac0cb71f85c08933ded170e371280287
ghtyuthg <ksjceniaareli01@hotmail.com>
2024-05-13 22:33:33 +0700

    add more features

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function countConsonants(str) {
-  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+function zigzagLevelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  let isReverse = false;
+  const queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (isReverse) {
+        level.unshift(node.val);
+      } else {
+        level.push(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+    isReverse = !isReverse;
+  }
+  return result;
 }
